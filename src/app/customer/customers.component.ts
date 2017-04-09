82b13c294e4b5f51529a02af0ecbf0642b96735e
akashmitra <akash.mitra@outlook.com>
2017-04-10 00:47:09 +0530

    Observable, Promises and Subscribe

--- src/app/customer/customers.component.ts
+++ src/app/customer/customers.component.ts
@@ -10,15 +10,41 @@ import { CustomerService } from './customer.service';
 })
 
 export class CustomersComponent implements OnInit {
-    customers: Observable<any[]>;
+    // customers: Promise<any[]>;
+
+    customers: any[];
 
     constructor(private _customerService: CustomerService) { }
 
     ngOnInit() {
-        this.customers = this._customerService.getCustomers()
-            .catch((err) => {
-                console.log(err);
-                return Observable.of(true);
-            });
+
+        // Rx Observable Version with Subscribe Function 
+        this._customerService.getCustomers_RxObservable()
+            .subscribe(
+            // Hey it worked
+            (customers) => this.customers = customers,
+            // No it didnt work
+            (err) => { console.log(err); }
+            );
+
+        // Straight Up Promise to an Array
+        // this._customerService.getCustomers()
+        // .then((customers) => this.customers = customers)
+        // .catch((err) => {
+        //     console.log(err);
+        // });
+
+        // Promise <any[]> Version
+        // this.customers = this._customerService.getCustomers()
+        // .catch((err) => {
+        //     console.log(err);
+        // });
+
+        // Rx Observable Version
+        // this.customers = this._customerService.getCustomers_RxObservable()
+        // .catch((err) => {
+        //     console.log(err);
+        //     return Observable.of(true);
+        // });
     }
 }   
